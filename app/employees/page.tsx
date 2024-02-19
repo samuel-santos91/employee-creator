48263854b6fb6bc3a612e9253ea9c9db1f11fd39
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-02-19 17:44:25 +1100

    fixes loader position

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -84,7 +84,7 @@ export default function Employees() {
         ))}
       </section>
 
-      <span className="relative top-1/2 text-center">
+      <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2">
         <PulseLoader loading={loading} />
       </span>
     </>
